a9631ce0e7b446cf8a8732e58801546aabc933fc
dekrish5612 <dekrish5697@gmail.com>
2024-11-10 21:31:06 +0530

    (add)

---
+++ Js basic/data type.js
@@ -0,0 +1,8 @@
+let message = ("hello world");
+let score = 100;
+let iscompleted = true
+let unknow;
+console.log (typeof message);
+console.log (typeof score);
+console.log (typeof iscompleted);
+console.log (typeof unknow);
