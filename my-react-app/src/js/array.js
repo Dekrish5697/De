37c5454844a4e7bb6d85f576a8d0fe23b4c3db2d
dekrish5612 <dekrish5697@gmail.com>
2024-11-10 21:44:29 +0530

    add

---
+++ my-react-app/src/js/array.js
@@ -0,0 +1,7 @@
+let person = { name: "John"} ;
+let age = 25;
+let numbers = [1, 2, 3, 4, 5,6];
+age = 60;
+console.log(person.name); // Output: John
+console.log(numbers[5]);  // Output: 6
+console.log(age);         // output: 60
---
+++ my-react-app/src/js/control flow.js
@@ -0,0 +1,7 @@
+let score = 85;
+if (score > 90) {
+console.log("Excellent");
+} else if (score > 70) {
+console.log("Good");
+} else {
+console.log("Needs Improvement");
